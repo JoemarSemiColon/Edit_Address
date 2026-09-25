5fc93af13c46bdee0bd146f99c99109e9762a777
RamRam <117159355+JoemarSemiColon@users.noreply.github.com>
2026-09-25 15:23:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+st.title("File Upload")
+
+with st.form("upload_form"):
+
+    uploaded_file = st.file_uploader(
+        "Upload Excel file",
+        type=["xlsx", "xls"]
+    )
+
+    submit = st.form_submit_button("Submit")
+
+if submit:
+
+    if uploaded_file is not None:
+
+        df = pd.read_excel(uploaded_file)
+
+        st.success("File submitted successfully!")
+
+        st.write("File name:", uploaded_file.name)
+
+
+        fullAdress = uploaded_file["AREA"]
+        st.dataframe(fullAdress)
+
+        
+
+    else:
+        st.warning("Please upload a file first.")
